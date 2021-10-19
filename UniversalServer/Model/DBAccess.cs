eef22ad8fae20889965d5c465add560774a08553
Rasic <parasic@emea.corpdir.net>
2021-10-19 09:00:06 +0200

    Initial commit

---
+++ UniversalServer/Model/DBAccess.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace DaimlerGmbHuCoKGServer.Model
+{
+    public delegate EventHandler ErrorEventHandler(string msg);
+    public class DBAccess
+    {
+        public void InsertData(TempValue t, HumidValue h, PressureValue p, DateTime dt, string ipa)
+        {
+
+            using (MySqlConnection con = new MySqlConnection(@"Data Source=127.0.0.1;Database=homeserverproject;User Id=PARASIC;Password=Admin1702;SSL Mode=0"))
+            {
+
+                string insert = "INSERT INTO values_table (Temperature, HumidValue, PressureValue, Date_Time, IPA) VALUES(@temp, @humidity, @pressure, @dateTime, @ipa)";
+
+                using (MySqlCommand command = new MySqlCommand(insert, con))
+                {
+                    command.Parameters.AddWithValue("@temp", t.Value + "°C");
+                    command.Parameters.AddWithValue("@humidity", h.Value + " g/m³");
+                    command.Parameters.AddWithValue("@pressure", p.Value + " hPa");
+                    command.Parameters.AddWithValue("@dateTime", dt);
+                    command.Parameters.AddWithValue("@ipa", ipa);
+
+                    con.Open();
+                    command.CommandText = insert;
+                    command.Connection = con; 
+                    int result = command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ UniversalServer/Model/HumidValue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaimlerGmbHuCoKGServer.Model
+{
+    public class HumidValue : ValuesBase
+    {
+        public override string ToString()
+        {
+            return $"Luftfeuchtigkeit: {Value}% am {DateAndTime}";
+        }
+    }
+}
